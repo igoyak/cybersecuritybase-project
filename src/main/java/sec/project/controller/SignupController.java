8326c0c3a7fe17ea573284157db32eb022f2b845
Gustav Larsson <gustav.larsson@aalto.fi>
2017-12-19 17:09:15 +0200

    Vulnerability 1: XSS

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import sec.project.domain.Signup;
 import sec.project.repository.SignupRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URLDecoder;
 
 @Controller
 public class SignupController {
@@ -14,9 +18,20 @@ public class SignupController {
     @Autowired
     private SignupRepository signupRepository;
 
-    @RequestMapping("*")
+    @RequestMapping("/")
     public String defaultMapping() {
         return "redirect:/form";
+    }
+
+
+    @RequestMapping("/**")
+    @ResponseBody
+    public String handle404(HttpServletRequest request) {
+        /*
+        Vulnerability 1: Reflected XSS
+         */
+        return "<h1>Not Found</h1><p>Sorry, could not find " + URLDecoder.decode(request.getRequestURI()) + "</p>";
+
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.GET)
--- src/test/java/sec/project/SampleTest.java
+++ src/test/java/sec/project/SampleTest.java
@@ -2,17 +2,22 @@ package sec.project;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import sec.project.repository.SignupRepository;
@@ -35,8 +40,17 @@ public class SampleTest {
     }
 
     @Test
+    public void vuln1XSS() throws Throwable {
+        String maliciousScript = "%3Cscript%3Ealert(document.cookie);%3C/script%3E";
+        MvcResult res = mockMvc.perform(get("/404" + maliciousScript)).andExpect(status().is2xxSuccessful()).andReturn();
+        String responseString = res.getResponse().getContentAsString();
+        assertTrue("A malicious script in the URL should be returned in the HTML response", responseString.contains(maliciousScript));
+    }
+
+    @Test
     public void signupAddsDataToDatabase() throws Throwable {
         mockMvc.perform(post("/form").param("name", "Testname").param("address", "Testaddress")).andReturn();
         assertEquals(1L, signupRepository.findAll().stream().filter(s -> s.getName().equals("Testname") && s.getAddress().equals("Testaddress")).count());
     }
+
 }
